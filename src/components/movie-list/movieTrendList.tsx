f9705a3351ec50b15fca850bcfd012535afc6e84
ANURAGTIGGA <a.aront003@gmail.com>
2023-11-26 11:08:02 +0530

    initial commit

---
+++ src/components/movie-list/movieTrendList.tsx
@@ -0,0 +1,23 @@
+import { Grid, Paper, Box } from "@mui/material";
+import { MovieDataType } from "../../assets/data";
+import MovieTrendCard from "../movie-card/movieTrend";
+
+interface MovieTrendListProps {
+  trendingList: MovieDataType[];
+}
+
+const MovietrendList = ({trendingList}:MovieTrendListProps) => {
+    return (
+        <Box sx={{ display: "flex", gap: 2, overflowX: "scroll" }}>
+            {trendingList.map((movie) => (
+                <Grid item key={movie.id}>
+                <Paper elevation={0} sx={{ backgroundColor: "transparent" }}>
+                    <MovieTrendCard movie={movie} />
+                </Paper>
+                </Grid>
+            ))}
+        </Box>
+    )
+};
+
+export default MovietrendList;
